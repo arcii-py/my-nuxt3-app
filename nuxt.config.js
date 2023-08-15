a9572fa01eb37a76496cdef266d4e055b26f0420
arcii-py <63791186+arcii-py@users.noreply.github.com>
2023-08-15 17:52:37 +0200

    remowed vite

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,13 +35,39 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
-    '@vite-pwa/nuxt',
+//    '@vite-pwa/nuxt',
   ],
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
+//    swURL: '/sw.js', // Path to our service worker
     manifest: {
+      name: "Nuxt 3 PWA",
       lang: 'en',
+      short_name: "Testing",
+      description: "test",
+      icons: [
+        {
+          src: 'icons/icon_64x64.png',
+          sizes: '64x64',
+          type: 'image/png'
+        },
+        {
+          src: 'icons/icon_144x144.png',
+          sizes: '144x144',
+          type: 'image/png'
+        },
+        {
+          src: 'icons/icon_192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'icons/icon_512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
     },
   },
 
