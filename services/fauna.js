18f5ecc1408cc178e9913123550819089861a9b4
arcii-py <63791186+arcii-py@users.noreply.github.com>
2023-08-16 17:00:19 +0200

    changed fauna seacret 2

--- services/fauna.js
+++ services/fauna.js
@@ -2,7 +2,7 @@ const faunadb = require('faunadb');
 
 const q = faunadb.query;
 const client = new faunadb.Client({
-  secret: process.env.FAUNA_SECRET
+  secret: process.env.FAUNA_SECRET3
 });
 
 export { q, client };
