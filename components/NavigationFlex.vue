06a3f8ce237da33c66efe3852cec385622bfad27
arcii-py <63791186+arcii-py@users.noreply.github.com>
2023-08-16 15:44:05 +0200

    added register link

--- components/NavigationFlex.vue
+++ components/NavigationFlex.vue
@@ -4,6 +4,7 @@
         <nav>
             <nuxt-link to="/">Home</nuxt-link>
             <nuxt-link to="/login">Login</nuxt-link>
+            <nuxt-link to="/register">Register</nuxt-link>
         </nav>
     </header>
 </template>
