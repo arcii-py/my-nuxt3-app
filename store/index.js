728ada1d5febb1f2f0d3b0734a58c033f20150c9
arcii-py <63791186+arcii-py@users.noreply.github.com>
2023-08-16 18:30:27 +0200

    added to store

---
+++ store/index.js
@@ -0,0 +1,12 @@
+export const state = () => ({
+    // your state properties here
+  });
+  
+  export const mutations = {
+    // your mutations here
+  };
+  
+  export const actions = {
+    // your actions here
+  };
+  
