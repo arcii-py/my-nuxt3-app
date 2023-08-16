90f75a2d008821ea13280e2314d43bdcc2cc08b8
arcii-py <63791186+arcii-py@users.noreply.github.com>
2023-08-16 18:50:07 +0200

    update

--- netlify/functions/register.js
+++ netlify/functions/register.js
@@ -1,27 +1,53 @@
-const faunadb = require('faunadb')
+const faunadb = require('faunadb');
+const q = faunadb.query;
 
-const client = new faunadb.Client({ secret: process.env.FAUNADB_SECRET })
-const q = faunadb.query
-
+const client = new faunadb.Client({
+  secret: process.env.FAUNADB_SECRET
+});
 
 exports.handler = async (event, context) => {
-  const { email, password } = JSON.parse(event.body)
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
 
+  const data = JSON.parse(event.body);
+
+  // Check if user already exists
   try {
-    const user = await client.query(
-      q.Create(q.Collection('users'), {
-        data: { email, password },
+    const existingUser = await client.query(
+      q.Get(q.Match(q.Index("users_by_email"), data.email))
+    );
+
+    if (existingUser) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: "User already exists" }),
+      };
+    }
+  } catch (error) {
+    // User not found is expected, so no action needed
+  }
+
+  // Create new user
+  try {
+    const newUser = await client.query(
+      q.Create(q.Collection("users"), {
+        data: {
+          email: data.email,
+          password: data.password, // Note: You should hash the password before storing
+          // ... any other data fields
+        },
       })
-    )
+    );
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ user: user.data }),
-    }
+      body: JSON.stringify(newUser),
+    };
   } catch (error) {
     return {
-      statusCode: 400,
-      body: JSON.stringify({ error: error.message }),
-    }
+      statusCode: 500,
+      body: JSON.stringify({ message: "Internal Server Error" }),
+    };
   }
-}
+};
