8a9e0819c082cfaa8077b436c0f2248e00eee1cf
arcii-py <63791186+arcii-py@users.noreply.github.com>
2023-08-16 19:22:07 +0200

    registration works, fixing login

--- netlify/functions/login.js
+++ netlify/functions/login.js
@@ -1,32 +1,52 @@
-const faunadb = require('faunadb')
+const faunadb = require('faunadb');
+const bcrypt = require('bcrypt');
+const q = faunadb.query;
 
-const client = new faunadb.Client({ secret: process.env.FAUNA_SECRET3 })
-const q = faunadb.query
-
+const client = new faunadb.Client({
+  secret: process.env.FAUNA_SECRET3
+});
 
 exports.handler = async (event, context) => {
-  const { email, password } = JSON.parse(event.body)
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+
+  const data = JSON.parse(event.body);
 
   try {
+    // Retrieve the user from FaunaDB
     const user = await client.query(
-      q.Get(q.Match(q.Index('users_by_email'), email))
-    )
+      q.Get(q.Match(q.Index("users_by_email"), data.email))
+    );
 
-    if (user.data.password !== password) {
+    // Compare the provided password with the hashed password in FaunaDB
+    const isPasswordValid = await bcrypt.compare(data.password, user.data.password);
+
+    if (!isPasswordValid) {
       return {
-        statusCode: 400,
-        body: JSON.stringify({ error: 'Invalid credentials' }),
-      }
+        statusCode: 401,
+        body: JSON.stringify({ message: "Invalid password" }),
+      };
     }
 
+    // Here, you would typically generate and return a JWT or some other form of token for authentication
+    // For simplicity, we're just returning a success message
     return {
       statusCode: 200,
-      body: JSON.stringify({ token: 'YOUR_TOKEN', user: user.data }),
-    }
+      body: JSON.stringify({ message: "Login successful" }),
+    };
+
   } catch (error) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({ error: error.message }),
+    if (error.name === 'NotFound') {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ message: "User not found" }),
+      };
     }
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Internal Server Error" }),
+    };
   }
-}
+};
